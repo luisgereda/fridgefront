6a32494398d65a57b496a9afdea1f9bee7afb375
luisgereda <luisgereda30@hotmail.com>
2021-06-29 15:54:15 +0200

    finished with the params

--- src/components/recipesCards/recipeCards.js
+++ src/components/recipesCards/recipeCards.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function RecipeCards(props) {
   return (
     <div>
       <h1>{props.label}</h1>
       <h2>{props.type}</h2>
-      <img src={props.image} alt={props.label}></img>
+      <Link to={`/recipes/${props.id}`}>
+        <img src={props.image} alt={props.label}></img>
+      </Link>
     </div>
   );
 }

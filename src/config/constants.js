d9de68489e8aa5d8e56d27e7528a7e0befa800df
gc0rtes <guilhermeco@gmail.com>
2021-06-29 13:55:52 +0200

    fix api url user store

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
-export const localhost = process.env.LOCAL_URL || "http://localhost:4000";
-export const api_url =
-  process.env.API_URL || "https://api.edamam.com/api/recipes/v2";
+export const apiUrl = process.env.LOCAL_URL || "http://localhost:4000";
+export const recipeApi =
+  process.env.recipeApi || "https://api.edamam.com/api/recipes/v2";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const app_id = "f6db72bb";
 export const app_key = "804e3743171e5052a44473490a615381";
